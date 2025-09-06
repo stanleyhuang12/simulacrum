7eeda3cebe6763bb5458075a0b7ee7122e9307c9
Stanley Huang <stanleyh201k@gmail.com>
2025-09-05 22:38:35 -0400

    new api endpts

--- legislative-simulacrum/src/App.svelte
+++ legislative-simulacrum/src/App.svelte
@@ -9,7 +9,7 @@
 
   //fetch root API and initalize a session cookie
   onMount(async () => {
-    await fetch("http://localhost:8000/", {
+    await fetch("https://zsvpuzru3l.execute-api.us-east-2.amazonaws.com/dev/legislative-simulacrum-09-05/", {
       credentials: "include" 
     });
   });
