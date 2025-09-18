f92b37785123f2b07503af942ba7ca1832ce00cc
Stanley Huang <stanleyh201k@gmail.com>
2025-09-18 12:30:11 -0400

    misc + cache

---
+++ app/src/routes/interface/+page.server.ts
@@ -0,0 +1,22 @@
+// src/routes/interface/+page.server.ts
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from "./$types";
+
+
+export const load: PageServerLoad = async( {cookies} )=>{
+    const sess_cookies = cookies.get('session-id-delibs');
+    console.log(sess_cookies)
+    const isUserValidated = await fetch("http://localhost:8000/trial-v1/delibs/validate-user", {
+        headers: {
+            "Cookie": `session-id-delibs=${sess_cookies}`
+        }
+    })
+
+    console.log(isUserValidated);
+    if (sess_cookies == null || !isUserValidated.ok) {
+        // No session cookie detected or match with PostgreSQL DB, redirect to login or show error
+        throw redirect(303, '/form');
+    }
+
+    return {sess_cookies: sess_cookies}; 
+}
