7519abfde7bd92f240ff0267cbde6fb4c1eb64c0
Stanley Huang <stanleyh201k@gmail.com>
2025-09-18 12:28:55 -0400

    correct cookie typo

--- app/src/routes/preamble/[session_details]/+page.server.ts
+++ app/src/routes/preamble/[session_details]/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from "./$types"
 export const load: PageServerLoad = async ({ params, url }) => { 
     //get cookie data 
     const cookieStr = params.session_details;
-    const match = cookieStr.match(/session_id_delibs=([^;]+)/);
+    const match = cookieStr.match(/session-id-delibs=([^;]+)/);
     const sess_cookies = match ? match[1] : null;
     console.log(sess_cookies)
 
