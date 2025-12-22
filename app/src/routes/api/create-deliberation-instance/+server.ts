c5ab4af9a655f0405004ca9d045d3f684167981c
Stanley Huang <stanleyh201k@gmail.com>
2025-12-22 14:13:45 -0500

    ssr create deliberations endpoint

--- app/src/routes/api/create-deliberation-instance/+server.ts
+++ app/src/routes/api/create-deliberation-instance/+server.ts
@@ -1,19 +1,20 @@
 import type { RequestHandler } from './$types';
 import { json, error, redirect } from '@sveltejs/kit';
-import { sequelize, DeliberationORM } from "$db/+server";
+import { DeliberationORM } from "$db/+server";
 
 
 /* GET request will retrieve the DeliberationORM object if it exists in the database  */
 
 export const POST: RequestHandler = async ( {cookies, request} ) => {
-    const userID = cookies.get('session-id-delibs'); 
+    const userID = await cookies.get('session-id-delibs'); 
     const res = await request.formData(); 
     
     if (!userID) { 
         redirect(400, "/form")
     }
 
-    const deliberation = await DeliberationORM.create(
+    try {
+        await DeliberationORM.create(
         {
             username: res.get('username'), 
             unique_id: userID, 
@@ -24,10 +25,11 @@ export const POST: RequestHandler = async ( {cookies, request} ) => {
             lawmaker_name: res.get('lawmaker_name'),
             /* No discussion history because this is initialization*/
         }
-    )
+    ) 
+    } catch(err: any) { 
+        return error(500, `Deliberation event not create in PostgreSQL ${err}`)
+    }
 
     console.log('Deliberation event created in PostgreSQL!')
-    
-    
-    return new Response("This is a vanilla text/chat completions API call.");
+    return json( { userID }, { status: 201 });
 }; 
