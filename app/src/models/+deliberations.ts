1900af89b185d982a9afd44550a83dbefb1fabf1
Stanley Huang <stanleyh201k@gmail.com>
2026-01-08 12:34:08 -0500

    update deliberation modules for memory management to ensure no pile up of system prompts and fix code for readability

--- app/src/models/+deliberations.ts
+++ app/src/models/+deliberations.ts
@@ -2,8 +2,10 @@ import { Simulacrum } from './+simulacrum';
 import type { ChatMessage, Dialogue } from './+utils';
 import { random_beta_sampler, ADVOCACY_GUARDRAILS } from './+utils';
 import { GuardrailsOpenAI, GuardrailTripwireTriggered } from "@openai/guardrails";
-import * as path from 'path';
 
+/**
+ * TYPES 
+ **/
 type VirtualLawmakerInstructionsTemplateType = Record<
   "support" | "support_with_caution" | "disagree_with_caution",
   string
@@ -12,7 +14,6 @@ type VirtualLawmakerInstructionsTemplateType = Record<
 type InitChatTemplate = Record<0|1|2, string>
 
 export type Memory = {
-    "prompt": ChatMessage[], 
     "dialogue": Dialogue, 
     "model": string,
     "episodeNumber": number
@@ -57,8 +58,10 @@ export class Lawmaker {
     public state: string;
     public ideology: string;
     public policy_topic: string;
+
     public degree_of_support?: number;
     public persona!: string; 
+
     public _memory: Array<Memory> = []; 
 
     constructor(
@@ -67,17 +70,16 @@ export class Lawmaker {
         state: string, 
         ideology: string, 
         policy_topic: string,
-        // degree_of_support: number | null = null,
     ) {
         this.advocateName = advocateName
         this.name = name;
         this.state = state;
         this.ideology = ideology;
         this.policy_topic = policy_topic;
-        this._init_virtual_lawmaker(); 
-        // this.degree_of_support = degree_of_support;
-    }
 
+        this._init_virtual_lawmaker(); 
+    }
+    /** MEMORY MANAGEMENT **/
     public _rehydrate_memory( savedMemory: Array<Memory> ) { 
         this._memory = savedMemory || []; 
     }
@@ -85,25 +87,8 @@ export class Lawmaker {
     public _retrieve_deserialized_memory() {
         return this._memory
     }
-
-    public log_episodal_memory(prompt: ChatMessage[], dialogue: Dialogue, model: string) {
-        /*
-        Encodes an episodal memory into the Lawmaker's memory stores. 
-        */
-        const memory: Memory = {
-            prompt: prompt, 
-            dialogue: dialogue, 
-            model: model, 
-            episodeNumber: this._memory.length + 1
-        };
-
-        this._memory.push(memory);
-    }
-
+    
     public retrieve_memory(memoryType: "short_term" | "long_term") {
-         /*
-        Retrieves short-term or long-term memory in the form a transcript.  
-        */
         if (this._memory.length == 0) {
             return ""
         }
@@ -126,61 +111,59 @@ export class Lawmaker {
         
     }
 
+    public log_episodal_memory(dialogue: Dialogue, model: string) {
+        const memory: Memory = {
+            dialogue: dialogue, 
+            model: model, 
+            episodeNumber: this._memory.length + 1
+        };
+
+        this._memory.push(memory);
+    }
+
+    /** PERSONA GENERATION **/
+
     public _virtual_lawmaker_bio() {
         return `I am ${this.name}, a ${this.ideology} lawmaker in the state of ${this.state}. 
         I will be discussing ${this.policy_topic} with you today.
         `;
     }
 
-    private _init_virtual_lawmaker(): string {
-        let supportValue: number; 
-        if (this.persona) { return this.persona}
-        if (this.degree_of_support == null ) {
-            this.degree_of_support = random_beta_sampler();
-            supportValue = this.degree_of_support
-        }  
+    private _init_virtual_lawmaker() {
+        this.degree_of_support = random_beta_sampler();
 
-        supportValue = this.degree_of_support
+        let key: keyof VirtualLawmakerInstructionsTemplateType;
+        if (this.degree_of_support <= 0.334) key = "support";
+        else if (this.degree_of_support <= 0.667) key = "support_with_caution";
+        else key = "disagree_with_caution";
 
-        type InstructionKey = keyof VirtualLawmakerInstructionsTemplateType;
-        let sampleKey: InstructionKey;
-
-        if (this.degree_of_support <=0.334 ) {
-            sampleKey = "support"
-        } else if (this.degree_of_support <= 0.667) {
-            sampleKey = "support_with_caution";
-        } else {
-            sampleKey = "disagree_with_caution";
-        };
-
-        this.persona = virtualLawmakerInstructionsTemplate[sampleKey];
-        
-        return this.persona
-    };
-
+        this.persona = virtualLawmakerInstructionsTemplate[key];
+    }
+    /*
+    LLM INTERACTION
+    */
     public async process(
         input: string,
         model: string = "gpt-4.1", 
     ) { 
-        /*
-        Method to interact with the server for LLM API call
-        */
         const systemInstructions: ChatMessage = {
             role: "system",
-            content: this.persona ? this.persona : this._init_virtual_lawmaker(),
+            content: this.persona,
         };
 
-        const memoryStores = this.retrieve_memory("long_term")
+        const memoryContext = this.retrieve_memory("long_term")
        
         const userInstructions: ChatMessage = {
             role: "user",
-            content: memoryStores + `${this.advocateName}: ${input}`
+            content:memoryContext
+            ? `${memoryContext}\n${this.advocateName}: ${input}`
+            : `${this.advocateName}: ${input}`
         };
 
         const messages: ChatMessage[] = [systemInstructions, userInstructions]
         
         try {
-            const agentResponse = await fetch("/api/llm-process", {
+            const response = await fetch("/api/llm-process", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
@@ -192,16 +175,16 @@ export class Lawmaker {
                 
                 });
 
-            const res = await agentResponse.json();
+            const res = await response.json();
             const text = res.choices[0].message.content;
             
-            /* Manage memory stores*/
             const currentDialogue: Dialogue = {
                 prompt: input,
                 response: text
             }
             
-            this.log_episodal_memory(messages, currentDialogue, model)
+            this.log_episodal_memory(currentDialogue, model)
+            
             return text;
         } catch(err) {
             return err;
@@ -213,47 +196,30 @@ export class Lawmaker {
 export class Deliberation extends Simulacrum {
     public ideology: string;
     public lawmaker_name: string;
-    public guard!: GuardrailsOpenAI; 
-    private guardPromise?: Promise<GuardrailsOpenAI>;
     public conversation_turn: number = 0; 
 
     constructor(
-        _username: string, 
-        _group: string,
-        _simulacrum_type: string = 'deliberations',
+        username: string, 
+        group: string,
+        simulacrum_type: string = 'deliberations',
         policy_topic: string,
         state: string,
         num_agents: number=1,
         ideology: string,
         lawmaker_name: string
     ) {
-        super(_username, _group, _simulacrum_type, policy_topic, state, num_agents); // call parent constructor first
+        super(username, group, simulacrum_type, policy_topic, state, num_agents); // call parent constructor first
         this.ideology = ideology;
         this.lawmaker_name = lawmaker_name;
-        this._init_virtual_lawmaker(); 
 
+        this._init_virtual_lawmaker(); 
     }
 
     public _init_virtual_lawmaker() {
         this.lawmaker = new Lawmaker(this._username, this.lawmaker_name, this.state, this.ideology, this.policy_topic)
     }
 
-    // private async _init_guard(): Promise<GuardrailsOpenAI> {
-    //     const guard = await GuardrailsOpenAI.create(
-    //         path.resolve("guardrails_config.json")
-    //     );
-    //     this.guard = guard; 
-    //     return guard;
-    // }
-    // private async getGuard(): Promise<GuardrailsOpenAI> {
-    //     if (this.guard) return this.guard; 
-    //     if (this.guardPromise) return await this.guardPromise; 
-    //     return await this._init_guard();
-    // }
-
     public async _guardrail_moderation(text: string) {
-        // const guard = await this.getGuard(); 
-
         const agentResponse = await fetch("/api/llm-process", {
             method: "POST",
             headers: {
@@ -271,45 +237,41 @@ export class Deliberation extends Simulacrum {
         } 
         
         return true 
-    }
-    
-
-    public initial_perma_template(
-        template_number: 0|1|2,
-    ) {
-        let templateText =  initChatTemplate[template_number]
+    };
+    private initial_template(n: 0|1|2,) {
+        let templateText =  initChatTemplate[n]
+        
         templateText = templateText
                 .replace("{username}", this._username)
                 .replace("{issue}", this.policy_topic);
 
         return templateText
-        };
-    
+    };
     public async panel_discussion(input: string) {
         const turn = this.conversation_turn;
-        this.conversation_turn += 1;
+        this.conversation_turn +1;
 
         if (turn <= 1) {
-            const text = this.initial_perma_template(turn as 0 | 1 | 2);
-            const prompt: ChatMessage = {
-                role: "user",
-                content: input 
-            }
-            const assistantMessage: ChatMessage = {
-                role: "assistant", 
-                content: text
-            }
-
+            const text = this.initial_template(turn as 0 | 1 | 2);
             const currentDialogue: Dialogue = {
                 prompt: input,
                 response: text
             }
             
-            this.lawmaker.log_episodal_memory([prompt, assistantMessage],currentDialogue, "automated_response")
+            this.lawmaker.log_episodal_memory(currentDialogue, "automated_response")
         }
 
+
         if (turn === 2) {
+
+            let userTranscript: string = ""; 
+            this._memory.forEach(item => {
+                userTranscript += item.dialogue.prompt.trim() + ""
+            });
+
+            userTranscript.trim(); 
             await this._guardrail_moderation(input); 
+
             return this.lawmaker.process(input)
         }
 
@@ -317,21 +279,30 @@ export class Deliberation extends Simulacrum {
     }
 }
 
-export function hydrateDeliberationInstance( deliberationRecord: any) { 
-    const deliberationObject = new Deliberation(
-        deliberationRecord.username, 
-        deliberationRecord.organization, 
+/* =========================
+   HYDRATION
+========================= */
+
+export function hydrateDeliberationInstance( record: any) { 
+    const d = new Deliberation(
+        record.username, 
+        record.organization, 
         "deliberations", 
-        deliberationRecord.policy_topic, 
-        deliberationRecord.state, 1,  
-        deliberationRecord.ideology,
-        deliberationRecord.lawmaker_name   
+        record.policy_topic, 
+        record.state,
+        1,  
+        record.ideology,
+        record.lawmaker_name   
     )
-    if (deliberationRecord.discussion_history == null) { return deliberationObject }
+    if (record.memory) {
+        d.lawmaker._rehydrate_memory(record.memory);
+    }
 
-    deliberationObject.lawmaker._rehydrate_memory(deliberationRecord.memory)
-    deliberationObject.lawmaker.persona = deliberationRecord.persona 
+    if (record.persona) {
+        d.lawmaker.persona = record.persona
+    }
 
-    return deliberationObject
+    return d
 }
+
 
