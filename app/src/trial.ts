3f3f70a0efa0fcad9dcc64875caec0fdf39726cc
Stanley Huang <stanleyh201k@gmail.com>
2026-01-11 17:15:43 -0500

    updates

---
+++ app/src/trial.ts
@@ -0,0 +1,13 @@
+import { Deliberation } from "$models/+deliberations"
+
+const ds = new Deliberation(
+    "Stanley Huang",
+    "STRIPED",
+    "deliberations",
+    "Out of Kids Hands campaign",
+    "Massachusetts",
+    1,
+    "very conservative",
+    "Representative John Doe"
+)
+
