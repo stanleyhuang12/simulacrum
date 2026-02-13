8fd420c4a9b5057ccfae2b100e7624f3badbdeff
Stanley Huang <stanleyh201k@gmail.com>
2026-02-13 13:16:59 -0500

    audio context manager

--- app/src/routes/reflection/+page.svelte
+++ app/src/routes/reflection/+page.svelte
@@ -1,4 +1,39 @@
+<script lang='ts'> 
 
+    /*Audio implementation details: 
+        The AudioStream is a stream of audio content. The Audio Context is an audio processing interface.
+        We create an Analyser node. The Audio Streams 
+    */
+    let audioAccept; 
+    let audioStream: MediaStream; 
+    const audioCtx = new AudioContext();
+    const audioAnalyser = audioCtx.createAnalyser();  // creates analyzer node `
+    
+    async function getAudioStream() {
+        try {
+            audioAccept = window.navigator.mediaDevices; 
+            if (!audioAccept || !audioAccept.getUserMedia) {
+                throw MediaError; 
+            }
+            audioStream = await window.navigator.mediaDevices.getUserMedia( { audio: true } ); 
+            console.log("Collecting audioStreams")
+
+        } catch(error) {
+            console.error(error)
+        }
+    }
+
+    async function pauseAudioStream() {
+        /* Pause audio stream temporarily. */
+    }
+
+    async function getAudioFeedback() {
+        const source = audioCtx.createMediaStreamSource(audioStream); 
+        source.connect(audioAnalyser); 
+
+    }
+
+</script>
 
 <div>
     <header> You have now chatted with a lawmaker. Let's debrief! </header>
@@ -10,10 +45,14 @@
         
       You can record the audio. 
     </div>
+    
     <div class="main-record-controls">
-      <button class="click-record" aria-label="record button"> Record </button>
+      <button class="click-record"onclick={getAudioStream} aria-label="record button"> Record </button>
       <button class="click-stop" aria-label="pause record button"> Pause </button>
     </div>
+    <section class="audio-signal"> 
+        
+    </section>
     <div class="main-record-cache-controls">
       <button class="reset-record" aria-label="reset recording"> Restart </button>
       <button class="submit-record" aria-label="submit recording"> Done </button>
