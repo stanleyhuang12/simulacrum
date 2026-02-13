59112faef992ff42c1593b0fbdc7ff1570878978
Stanley Huang <stanleyh201k@gmail.com>
2026-02-13 11:53:45 -0500

    some documentation for feedback module improvement

--- app/src/routes/feedback/[session_id]/+page.server.ts
+++ app/src/routes/feedback/[session_id]/+page.server.ts
@@ -3,11 +3,16 @@ import { redirect } from "@sveltejs/kit";
 import { validateAndRetrieveDeliberation } from "$db/+server";
 
 export const load: PageServerLoad =  async ( {cookies} ) => {
+    /*
+    Data load function for Feedback agent mapper, synthesis, and generation. 
+    The load function retrieves the session cookies, fetches records from the server, 
+    and stores the dialogue. 
+    */
     console.log("Running load function for feedback")
-    const sess_cookies = cookies.get('session-id-delibs'); 
-    console.log(sess_cookies);
+    const sessCookies = cookies.get('session-id-delibs'); 
+    console.log(sessCookies);
 
-    const deliberationRecord = await validateAndRetrieveDeliberation(sess_cookies)
+    const deliberationRecord = await validateAndRetrieveDeliberation(sessCookies)
     
     if (deliberationRecord == null) {
         redirect(308, "/")
@@ -17,7 +22,7 @@ export const load: PageServerLoad =  async ( {cookies} ) => {
     const savedMemory = deliberationRecord.toJSON().memory
     return { 
         memory: savedMemory,
-        sess_cookies: sess_cookies
+        sess_cookies: sessCookies
     }
     
 }
