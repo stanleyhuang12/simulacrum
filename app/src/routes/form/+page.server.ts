d0f5c0cc028993d23afdbdb262c0640ebbf4d064
Stanley Huang <stanleyh201k@gmail.com>
2026-03-09 10:58:13 -0400

    feat: support some demo operations

--- app/src/routes/form/+page.server.ts
+++ app/src/routes/form/+page.server.ts
@@ -4,23 +4,26 @@ import { fail } from '@sveltejs/kit';
 
 
 
-export const load: PageServerLoad = async ( { cookies } ) => {
-    const userID = cookies.set("session-id-delibs", crypto.randomUUID(), {path: "/", maxAge: 7200})
-    return userID 
+export const load: PageServerLoad = async ( event ) => {
+    const userID = event.cookies.set("session-id-delibs", crypto.randomUUID(), {path: "/", maxAge: 7200})
+    return userID
 }
+
 function validateEmail(email:  string): boolean {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email.toLowerCase());
 }
 
 export const actions: Actions = {
+    
     submit: async (event) => {
         const sess_cookies = await event.cookies.get("session-id-delibs")
         const formData = await event.request.formData();
         
         const payload = Object.fromEntries(formData); // Convert FormData -> object
-        
-        const email = payload.email.toString(); 
+        const payloadStr = JSON.stringify(payload); 
+        console.log(payload)
+        const email = payload.userEmail.toString(); 
         let is_missing = []; 
 
         for (const [k, v] of Object.entries(payload)) {
@@ -30,9 +33,14 @@ export const actions: Actions = {
         }; 
 
         if (!validateEmail(email)) {
-                fail(400, {"is_invalid": "email", ...( is_missing.length && { is_missing })})
+                return fail(400, {"is_invalid": "email", ...( is_missing.length && { is_missing })})
         }; 
-
+        
+        console.log(event.url)
+        if (event.url.searchParams.get('demo') == "true") {
+            const dataString = encodeURIComponent(payloadStr); 
+            redirect(303,  `/preamble/session_id_delibs=${sess_cookies}?data=${dataString}?demo=true`)
+        }
 
         const res = await event.fetch("/api/create-deliberation-instance", {
             method: "POST",
@@ -41,16 +49,16 @@ export const actions: Actions = {
                 'Content-Type': 'application/json',
                 'Cookie': `session-id-delibs=${sess_cookies}`
             },
-            body: JSON.stringify(payload),
+            body: payloadStr,
             credentials: "include"
         });
 
         if (!res.ok) {
             const err = await res.json();
             console.error("Server rejected payload:", err);
-            return fail(400, );
+            return fail(400, `Server rejected the payload: ${err}`);
         } else { 
-            const dataString = encodeURIComponent(JSON.stringify(payload));
+            const dataString = encodeURIComponent(payloadStr);
             console.log(dataString)
             redirect(303, `/preamble/session_id_delibs=${sess_cookies}?data=${dataString}`)
         }
