fcdebc63d9c68722042c91ad89e6502a7b3614bf
Stanley Huang <stanleyh201k@gmail.com>
2026-03-12 14:39:42 -0400

    add isDemo param

--- app/src/routes/reflection/+page.server.ts
+++ app/src/routes/reflection/+page.server.ts
@@ -2,12 +2,12 @@ import type { PageServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
 
-export const load: PageServerLoad = async ( event ): Promise<({userID: string})> => {
+export const load: PageServerLoad = async ( event ): Promise<({userID: string, isDemo: boolean})> => {
     const userID = event.cookies.get("session-id-delibs");
     const isDemo = event.url.searchParams.get('demo') == "true"; 
     if (!userID) { 
-        if (isDemo) { return {userID: "demo-key"} } else { throw redirect(404, "/form") }
+        if (isDemo) { return {userID: "demo-key", isDemo: true} } else { throw redirect(404, "/form") }
     }
 
-    return {userID: userID}; 
+    return {userID: userID, isDemo: isDemo}; 
 }
